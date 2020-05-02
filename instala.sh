392fc7761eef97091226b99e838e67049d5054c4
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-05-02 14:38:31 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-apt-get update -y; apt-get upgrade -y; wget -q https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/cgh.sh; chmod 777 cgh.sh* && ./cgh.sh*
+apt-get update -y; apt-get upgrade -y;
+update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
+dom='base64 -d'
+RE=$(echo $update1|$dom)
+wget -O cgh.sh $RE;chmod 777 cgh.sh* && ./cgh.sh*
