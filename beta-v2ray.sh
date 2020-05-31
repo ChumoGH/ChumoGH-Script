07dbf61d26a201695aabe212a3b90ef18aba4b91
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-05-31 09:58:50 -0500

    Update beta-v2ray.sh

--- beta-v2ray.sh
+++ beta-v2ray.sh
@@ -1,2 +1,2 @@
 source <(curl -sL https://multi.netlify.com/v2ray.sh)
-source <(curl -sL https://multi.netlify.com/v2ray.sh) --remove
+#source <(curl -sL https://multi.netlify.com/v2ray.sh) --remove
