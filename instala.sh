2020f39b8e8eeae7dc357e44a4e5ef1e688e0267
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-05 00:23:43 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,4 +1,5 @@
-apt-get update -y; apt-get upgrade -y;
+apt-get update -y 1> /dev/null 2> /dev/null
+apt-get upgrade -y 1> /dev/null 2> /dev/null
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
