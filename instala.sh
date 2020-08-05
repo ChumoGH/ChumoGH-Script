cc147e7255aa9b96958392a690f68b754a082343
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-05 01:22:54 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,3 +1,4 @@
+echo -e ' TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!'
 apt-get update -y 1> /dev/null 2> /dev/null
 apt-get upgrade -y 1> /dev/null 2> /dev/null
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
