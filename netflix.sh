c3e030f3a8fb411cbd3cd7ab67aed4c6f7bbb40a
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-07 18:43:47 -0500

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+echo -ne "\033[1;33m ["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m++"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "\033[1;33m ["
+done
+echo -e "\033[1;33m]\033[1;31m -\033[1;32m 100 %\033[1;37m"
+}
+
 fun_bar 'apt install cmake -y'
 wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
 tar xf badvpn-1.999.128.tar.bz2
