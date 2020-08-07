8c5ce2674c23fbc99caad3d61cbb77274c2524d4
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-07 18:23:22 -0500

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt install cmake -y
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
+fun_bar 'apt install cmake -y'
+wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
 tar xf badvpn-1.999.128.tar.bz2
 #cd badvpn-1.999.128/
 cd /etc/adm-lite/badvpn-1.999.128/
@@ -9,4 +9,5 @@ cmake /etc/adm-lite/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=
 make install
 badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &
 #echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &' >> /etc/rc.local
+rm -rf badvpn-1.*
 
