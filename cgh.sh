b503b6a84cfa67a5459af07c4918b89aa0dc8746
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-11 13:01:55 -0500

    Update cgh.sh

--- cgh.sh
+++ cgh.sh
@@ -138,6 +138,22 @@ echo "desactivado" > /bin/ejecutar/val1
 echo -e "${cor[5]}NOMBRE AGREGADO EXITOSAMENTE"
 echo -ne " \033[0m"
 clear
+if cat /root/.bashrc | grep ChumoGH; then
+echo -e ":)"
+else
+echo "clear" >> .bashrc
+echo 'DATE=$(date +"%d-%m-%y")' >> .bashrc
+echo 'TIME=$(date +"%T")' >> .bashrc
+echo 'figlet -k ☆ChumoGH☆ | lolcat' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e "Nombre del Servidor : $HOSTNAME"' >> .bashrc
+echo 'echo -e "Fecha del Servidor : $DATE"' >> .bashrc
+echo 'echo -e "Hora del Servidor : $TIME"' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e "Bienvenido!"' >> .bashrc
+echo 'echo -e "Teclee cgh o menu para Entrar al MENU."' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+fi
 cowsay -f stegosaurus "BIENVENIDO Y GRACIAS POR UTILIZAR  ⌐╦╦═─ ☆ChumoGH☆ ADM SCRIPT "| lolcat
 echo -e "${cor[5]}=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
 echo -e "${cor[3]} $(source trans -b pt:${id} "INSTALACION DE ChumoGH-Pannel Instalada Correctamente!")"
