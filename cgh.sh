ceea4c67df80679ee1b2ec81bfaf96497bd9e321
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-14 14:09:02 -0500

    Update cgh.sh

--- cgh.sh
+++ cgh.sh
@@ -3,6 +3,34 @@ fecha=`date`;
 fun_bar1 () {
 comando[0]="$1"
 comando[1]="$2"
+(
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+) > /dev/null 2>&1 &
+tput civis
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+while true; do
+for((i=0; i<18; i++)); do
+echo -ne "\033[1;31m#"
+sleep 0.1s
+done
+[[ -e $HOME/fim ]] && rm $HOME/fim && break
+echo -e "\033[1;33m]"
+sleep 1s
+tput cuu1
+tput dl1
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+
+
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
  (
 [[ -e $HOME/fim ]] && rm $HOME/fim
 ${comando[0]} -y > /dev/null 2>&1
