c0cb06fce272a3ae0ba80090e996e314c6da02eb
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-14 15:25:13 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -54,4 +54,4 @@ fun_bar1 'apt-get update -y' 'apt-get upgrade -y'
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
-wget -O cgh.sh $RE;chmod 777 cgh.sh* && ./cgh.sh*;
+wget -O cgh.sh $RE;chmod 777 cgh.sh* && ./cgh.sh*
