87e2190137385a10b2a0fe0d15b8b9b8ef8a8851
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-14 13:25:34 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -21,9 +21,37 @@ while true; do
    echo -ne "\033[1;33m ["
 done
 }
+
+fun_bar1 () {
+comando[0]="$1"
+comando[1]="$2"
+(
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+) > /dev/null 2>&1 &
+tput civis
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+while true; do
+for((i=0; i<18; i++)); do
+echo -ne "\033[1;31m#"
+sleep 0.1s
+done
+[[ -e $HOME/fim ]] && rm $HOME/fim && break
+echo -e "\033[1;33m]"
+sleep 1s
+tput cuu1
+tput dl1
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+clear
 echo -e ' TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!'
-fun_bar 'apt-get update -y' 'apt-get upgrade -y'
+fun_bar1 'apt-get update -y' 'apt-get upgrade -y'
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
-wget -O cgh.sh $RE;chmod 777 cgh.sh* && ./cgh.sh*
+wget -O cgh.sh $RE;chmod 777 cgh.sh* && ./cgh.sh*;
