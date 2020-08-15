3fd6ef56f351706f666ba94ad647e42be7032b7b
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-15 00:42:40 -0500

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -39,7 +39,7 @@ apt-get install cmake -y 1>/dev/null 2>/dev/null
 apt-get install screen wget gcc build-essential g++ make -y 1>/dev/null 2>/dev/null
 wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
 tar xf badvpn-1.999.128.tar.bz2
-cd badvpn-1.999.128/
+cd /etc/adm-lite/badvpn-1.999.128/
 cmake ~/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 make install
 echo "Ejecutando BadVPN en puerto 7300"
