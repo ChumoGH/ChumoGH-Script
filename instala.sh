7c07529ac96c3856bab9a6bdd96d4a0772b2df09
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-10-05 08:36:26 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,4 +1,5 @@
 killall apt apt-get &> /dev/null
+rm -rf cgh.*
 dpkg --configure -a
 fun_bar3 () {
 comando="$1"
@@ -76,9 +77,12 @@ echo -e ' TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!'
 echp 
 fun_bar1 'apt-get update -y' 'apt-get upgrade -y'
 echo 
-fun_bar 'apt-get install software-properties-common -y' 'apt-add-repository universe -y'
-echo 
-fun_bar3 'sudo add-apt-repository ppa:neurobin/ppa -y' 'sudo apt-get update -y 'sudo apt-get install shc -y'
+fun_bar 'apt-get install software-properties-common -y' 
+fun_bar 'apt-add-repository universe -y'
+fun_bar 'sudo add-apt-repository ppa:neurobin/ppa -y' 
+fun_bar3 'sudo apt-get install shc -y'
+echo -ne 'Listo!!! TERMINAMOS DE ACTUALIZAR TODOS LOS REPOSITORIOS'
+sleep 2s
 rm -rf /etc/localtime &>/dev/null
 ln -s /usr/share/zoneinfo/America/Guayaquil /etc/localtime &>/dev/null
 rm $(pwd)/$0 &> /dev/null
