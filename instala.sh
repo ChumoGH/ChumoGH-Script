f53454df10d878534c22d13627059990054f480c
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-10-15 12:32:24 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -74,24 +74,23 @@ echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
 tput cnorm
 }
 update_pak () {
+echo -ne " \033[1;31m[ ! ]  TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!  [ ! ]"
+fun_bar1 'apt-add-repository universe -y'
+sudo add-apt-repository ppa:neurobin/ppa -y &>/dev/null
 echo -ne " \033[1;31m[ ! ] apt-get update"
-fun_bar 'apt-get update -y' echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+apt-get update -y echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
 echo -ne " \033[1;31m[ ! ] apt-get upgrade"
-fun_bar 'apt-get upgrade -y' echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+apt-get upgrade -y echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
 echo -ne " \033[1;31m[ ! ] apt-get Repositorios Universales"
 apt-get install software-properties-common -y > /dev/null 2>&1 && echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+echo -e 'Listo!!! TERMINAMOS DE ACTUALIZAR TODOS LOS REPOSITORIOS'
 return
 }
 clear
-echo -e ' TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!'
-echo -e
 update_pak
-fun_bar 'apt-add-repository universe -y'
-echo -e
-fun_bar 'sudo add-apt-repository ppa:neurobin/ppa -y' 
-echo -e 'Listo!!! TERMINAMOS DE ACTUALIZAR TODOS LOS REPOSITORIOS'
 sleep 2s
 rm -rf /etc/localtime &>/dev/null
+echo -e 'ZONA HORARIA EN UTC - 05:00 America/Guayaquil'
 ln -s /usr/share/zoneinfo/America/Guayaquil /etc/localtime &>/dev/null
 rm $(pwd)/$0 &> /dev/null
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvQ2h1bW9HSC1TY3JpcHQvbWFzdGVyL2NnaC5zaA=='
