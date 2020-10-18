8b64fb7968a518ea9940572c3d7e553eacc759d9
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-10-18 18:12:44 -0500

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
+
 fun_bar () {
 comando[0]="$1"
 comando[1]="$2"
- (
+(
 [[ -e $HOME/fim ]] && rm $HOME/fim
 ${comando[0]} -y > /dev/null 2>&1
 ${comando[1]} -y > /dev/null 2>&1
 touch $HOME/fim
- ) > /dev/null 2>&1 &
-echo -ne "\033[1;33m ["
+) > /dev/null 2>&1 &
+tput civis
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
 while true; do
-   for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m##"
-   sleep 0.1s
-   done
-   [[ -e $HOME/fim ]] && rm $HOME/fim && break
-   echo -e "\033[1;33m]"
-   sleep 1s
-   tput cuu1
-   tput dl1
-   echo -ne "\033[1;33m ["
+for((i=0; i<18; i++)); do
+echo -ne "\033[1;31m#"
+sleep 0.1s
 done
-echo -e "\033[1;33m]\033[1;31m -\033[1;32m 100%\033[1;37m"
+[[ -e $HOME/fim ]] && rm $HOME/fim && break
+echo -e "\033[1;33m]"
+sleep 1s
+tput cuu1
+tput dl1
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m EXITO !\033[1;37m"
+tput cnorm
 }
 
 ssl_redir() {
