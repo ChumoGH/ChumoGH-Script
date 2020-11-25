bdf5aa3d224c2efbb24e5189413af10bb5954163
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-11-25 13:02:33 -0500

    Update hora.sh

--- hora.sh
+++ hora.sh
@@ -23,6 +23,7 @@ echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\
 sleep 1s
 }
 act_hora () {
+echo "America/Chihuahua"
 echo "America/Chihuahua" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Chihuahua /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -30,6 +31,7 @@ echo -e "$barra"
 menu
 }
 act_hora1 () {
+echo "America/Mexico_City"
 echo "America/Mexico_City" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Mexico_City /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -37,6 +39,7 @@ echo -e "$barra"
 menu
 }
 act_hora2 () {
+echo "America/Hermosillo"
 echo "America/Hermosillo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Hermosillo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -44,6 +47,7 @@ echo -e "$barra"
 menu
 }
 act_hora3 () {
+echo "America/Santiago"
 timedatectl > /dev/null 2>&1
 timedatectl list-timezones > /dev/null 2>&1
 timedatectl list-timezones  | grep Santiago > /dev/null 2>&1
@@ -52,6 +56,7 @@ echo -e "$barra"
 menu
 }
 act_hora4 () {
+echo "America/Los_Angeles"
 timedatectl > /dev/null 2>&1
 timedatectl list-timezones > /dev/null 2>&1
 timedatectl list-timezones  | grep Los_Angeles > /dev/null 2>&1
@@ -60,6 +65,7 @@ echo -e "$barra"
 menu
 }
 act_hora5 () {
+echo "America/New_York"
 echo "America/New_York" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -67,6 +73,7 @@ echo -e "$barra"
 menu
 }
 act_hora6 () {
+echo "America/Guatemala"
 echo "America/Guatemala" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Guatemala /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -74,6 +81,7 @@ echo -e "$barra"
 menu
 }
 act_hora7 () {
+echo "America/Bogota"
 echo "America/Bogota" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Bogota /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
@@ -81,10 +89,18 @@ echo -e "$barra"
 menu
 }
 act_hora8 () {
+echo "America/Guayaquil"
 echo "America/Guayaquil" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Guayaquil /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1 && echo -e "\033[1;32m [OK]" || echo -e "\033[1;31m [FAIL]"
 echo -e "$barra"
+menu
+}
+
+verzm () {
+timedatectl
+echo -e "\033[1;31m PRESIONE ENTER PARA CONTINUAR \033[0m"
+read -p " "
 menu
 }
 shadowe_fun () {
@@ -101,8 +117,9 @@ echo -e "${cor[4]} [6] > ${cor[5]}ACTUALIZAR HORARIO  America/New_York"
 echo -e "${cor[4]} [7] > ${cor[5]}ACTUALIZAR HORARIO  America/Guatemala"
 echo -e "${cor[4]} [8] > ${cor[5]}ACTUALIZAR HORARIO  America/Bogota"
 echo -e "${cor[4]} [9] > ${cor[5]}ACTUALIZAR HORARIO  ECUADOR "
+echo -e "${cor[4]} [10] > ${cor[7]}VER ZONA HORARIA ACTUAL "
 echo -e "${cor[4]} [0] > ${cor[0]}SALIR\n${barra}"
-while [[ ${opx} != @(0|[1-9]) ]]; do
+while [[ ${opx} != @(0|[1-10]) ]]; do
 echo -ne "${cor[0]}Digite una Opcion: \033[1;37m" && read opx
 tput cuu1 && tput dl1
 done
@@ -110,33 +127,44 @@ case $opx in
 	0)
 	exit;;
 	1)
+	fun_bar
 	act_hora
 	break;;
 	2)
+	fun_bar
 	act_hora1
 	break;;
 	3)
+	fun_bar
 	act_hora2
 	break;;
    	 4)
+	 fun_bar
 	act_hora3
 	break;;
 	 5)
+	 fun_bar
 	act_hora4
 	break;;
 	 6)
+	 fun_bar
 	act_hora5
 	break;;
 	 7)
+	 fun_bar
 	act_hora6
 	break;;
 	 8)
+	 fun_bar
 	act_hora7
 	break;;
 	 9)
+	 fun_bar
 	act_hora8
 	break;;
-  
+	10)
+	verzm
+  	break;;
 esac
 done
 }
