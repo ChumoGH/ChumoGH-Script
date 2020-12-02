81076ea13d700fc5064b97abf86a0e32f2465b02
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-02 17:17:33 -0500

    Update key.sh

--- key.sh
+++ key.sh
@@ -2,7 +2,8 @@
 SCPdir="/etc/cgh" && [[ ! -d ${SCPdir} ]] && mkdir  ${SCPdir}
 cd /etc/cgh
 rm -f manager*
-wget -q https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/manager
-cat manager > /bin/gerar
+#wget -q https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/manager
+#cat manager> /bin/gerar
+echo 'source <(curl -sL https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/manager)' > /bin/gerar
 chmod +x /bin/gerar
 rm -f key.*
