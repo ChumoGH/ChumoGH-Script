d50099b31f9a61e572fc770eeec8af7e63245a4a
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-02 07:37:31 -0500

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -27,12 +27,16 @@ echo -e "\033[1;33m]\033[1;31m EXITO -\033[1;32m 100 %\033[1;37m"
 fun_bar 'apt install cmake -y'
 #fun_bar 'apt-get install screen wget gcc build-essential g++ make -y'
 rm -f badvpn-1.999.128.tar.*
+cd /etc/adm-lite/
+rm -f badvpn-1.999.128.tar.*
+rm -rf badvpn-1.999.128/
 wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
 tar xf badvpn-1.999.128.tar.bz2
-cd /etc/adm-lite/badvpn-1.999.128/
+cd /etc/adm-lite/badvpn-1.999.128
 cmake /etc/adm-lite/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 make install
 badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &
+echo 'badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/nul &' > /etc/rc.local
 #echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &' >> /etc/rc.local
 cd /etc/adm-lite/
 rm -f badvpn-1.999.128.tar.*
