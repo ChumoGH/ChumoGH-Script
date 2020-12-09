a3eb9a0fed3a65602c104da0935a56649d682702
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-09 17:49:48 -0500

    Update insta.sh

--- Key-gerar/insta.sh
+++ Key-gerar/insta.sh
@@ -11,7 +11,50 @@ cor[2]="\033[1;32m"
 cor[3]="\033[1;31m"
 cor[4]="\033[1;33m"
 cor[0]="\033[1;37m"
-
+fun_bar1 () {
+comando[0]="$1"
+comando[1]="$2"
+(
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+) > /dev/null 2>&1 &
+tput civis
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+while true; do
+for((i=0; i<18; i++)); do
+echo -ne "\033[1;31m≛"
+sleep 0.1s
+done
+[[ -e $HOME/fim ]] && rm $HOME/fim && break
+echo -e "\033[1;33m]"
+sleep 1s
+tput cuu1
+tput dl1
+echo -ne "  \033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m EXITO !\033[1;37m"
+tput cnorm
+}
+update_pak () {
+echo -ne " \033[1;31m[ ! ]  TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!  [ ! ]"
+echo -e ' '
+fun_bar1 'apt-add-repository universe -y'
+echo -e ' '
+fun_bar1 'sudo add-apt-repository ppa:neurobin/ppa'
+echo -ne " \033[1;31m[ ! ] apt-get update"
+apt-get update -y > /dev/null 2>&1 && echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+echo -ne " \033[1;31m[ ! ] apt-get upgrade"
+apt-get upgrade -y > /dev/null 2>&1 && echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+echo -ne " \033[1;31m[ ! ] apt-get Repositorios Universales"
+apt-get install software-properties-common -y > /dev/null 2>&1 && echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
+echo -e 'Listo!!! TERMINAMOS DE ACTUALIZAR TODOS LOS REPOSITORIOS'
+clear
+return
+}
+clear
+update_pak
 #TEXTOS
 txt[206]="Archivito(s) Actualizado(s)!"
 txt[208]="Invalid Key, Contact the Script Admin!"
