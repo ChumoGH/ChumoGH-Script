a0c24b1b3246f317a5992252cb7c544e80c7e0a7
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-18 10:21:06 -0500

    Update gerar.sh

--- Key-gerar/gerar.sh
+++ Key-gerar/gerar.sh
@@ -23,48 +23,13 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 
 ####inicio puertos
 ports_ () {
-
-portas_var="/tmp/portas"
-porta_var="/tmp/portas2"
-lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN" > $portas_var
-while read port; do
-var1=$(echo $port | awk '{print $1}')
-var2=$(echo $port | awk '{print $9}' | awk -F ":" '{print $2}')
- if [ ! -e "$porta_var" ]; then
- echo -e "$var1 $var2" > $porta_var
- fi
- if [ "$(cat $porta_var | grep "$var1" | grep "$var2")" = "" ]; then
- echo -e "$var1 $var2" >> $porta_var
- fi
-done < $portas_var
-
-i=1
-while true; do
-a=$(sed -n "$i"p $porta_var | awk '{print $1}')
-b=$(sed -n "$i"p $porta_var | awk '{print $2}')
-#[[ "$a" != "" ]] && texto="\033[1;34m ▸ \033[1;37m$a: \033[1;31m$b" || texto=""
-[[ "$a" = "" ]] && break
-i=$(($i+1))
-c=$(sed -n "$i"p $porta_var | awk '{print $1}')
-d=$(sed -n "$i"p $porta_var | awk '{print $2}')
-[[ "$c" != "" ]] && texto_="\033[1;34m ◂ \033[1;37m$c: \033[1;31m$d" || texto_=""
-[[ "$a" = "" ]] && break
-if [[ $a = "apache2" ]];then
-echo -e "\033[1;34m ▸ \033[1;37mAPACHE: \033[1;31m$b"
-fi
-i=$(($i+1))
-#echo -e "$texto  $texto_"
-done
-rm $portas_var
-rm $porta_var
-
 porta=`if netstat -tunlp |grep nc.traditional 1> /dev/null 2> /dev/null; then
 echo -e "\033[0;36mKeygen :\033[0;35m 8888"
 fi`;
 portaa=`if netstat -tunlp |grep apache2 1> /dev/null 2> /dev/null; then
 echo -e "\033[0;36mApache2 :\033[0;35m 81"
 fi`;
-echo -ne "$(msg -verd "     Keys Usadas") $(msg -verm2 " : ") " && msg -bra "\033[1;41m $(cat $IVAR) " $porta
+echo -ne "$(msg -verd "     Keys Usadas") $(msg -verm2 " : ") " && msg -bra "\033[1;41m $(cat $IVAR) $porta"
 }
 ####fin puertos
 
