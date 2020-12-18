9ee228bcce2c6b4c7f7f90224ce733f1271f7cbc
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-18 10:22:26 -0500

    Update gerar.sh

--- Key-gerar/gerar.sh
+++ Key-gerar/gerar.sh
@@ -24,12 +24,12 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 ####inicio puertos
 ports_ () {
 porta=`if netstat -tunlp |grep nc.traditional 1> /dev/null 2> /dev/null; then
-echo -e "\033[0;36mKeygen :\033[0;35m 8888"
+echo -e "\033[0;36m Keygen :\033[0;35m 8888"
 fi`;
 portaa=`if netstat -tunlp |grep apache2 1> /dev/null 2> /dev/null; then
 echo -e "\033[0;36mApache2 :\033[0;35m 81"
 fi`;
-echo -ne "$(msg -verd "     Keys Usadas") $(msg -verm2 " : ") " && msg -bra "\033[1;41m $(cat $IVAR) $porta"
+echo -ne "$(msg -verd "     Keys Usadas") $(msg -verm2 " : ") " && msg -bra "\033[1;41m $(cat $IVAR) $porta\n"
 }
 ####fin puertos
 
