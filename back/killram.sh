648c6b7f6a9c73548b45b268b3a163755bf1faa5
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-23 15:06:07 -0500

    Update killram.sh

--- back/killram.sh
+++ back/killram.sh
@@ -1,7 +1,7 @@
 id1=$(free -h | grep -i mem | awk {'print $4'})
 idM=$(echo $id1 |sed 's/[A-Z]//g')
 idm=$(echo $idM |sed 's/[a-z]//g')
-n=150
+n=90
 tiempo=$(printf '%(%D-%H:%M:%S)T') 
 if [ "$n" -gt "$idm" ]; then 
 echo $idm "Limpiando y Reiniciado por sobreConsumo el " $tiempo
