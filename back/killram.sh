d486e865b39f48bbb920bd0caed95fc1b4e80666
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-23 17:20:06 -0500

    Update killram.sh

--- back/killram.sh
+++ back/killram.sh
@@ -1,11 +1,14 @@
+#!/bin/sh
+#Autor: Henry Chumo 
+#Alias : ChumoGH
 id1=$(free -h | grep -i mem | awk {'print $4'})
 idM=$(echo $id1 |sed 's/[A-Z]//g')
 idm=$(echo $idM |sed 's/[a-z]//g')
 n=90
 tiempo=$(printf '%(%D-%H:%M:%S)T') 
 if [ "$n" -gt "$idm" ]; then 
-echo $idm "Limpiando y Reiniciado por sobreConsumo el " $tiempo >> /root/lm.log
+echo $idm "Mbs  -  Limpiando y Reiniciado el " $tiempo >> /root/lm.log
 source <(curl -sL https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/gnula.sh)
 else 
-echo $n " Esta bajo el limite en " $tiempo >> /root/lm.log
+echo $n "Mbs - Esta bajo el limite " $id1 " el " $tiempo >> /root/lm.log
 fi
