5a70d27ec77ba1d9f7d19683f61c88810b1c7f69
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-23 15:04:00 -0500

    Create killram.sh

---
+++ back/killram.sh
@@ -0,0 +1,11 @@
+id1=$(free -h | grep -i mem | awk {'print $4'})
+idM=$(echo $id1 |sed 's/[A-Z]//g')
+idm=$(echo $idM |sed 's/[a-z]//g')
+n=150
+tiempo=$(printf '%(%D-%H:%M:%S)T') 
+if [ "$n" -gt "$idm" ]; then 
+echo $idm "Limpiando y Reiniciado por sobreConsumo el " $tiempo
+source <(curl -sL https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/gnula.sh)
+else 
+echo $n " Esta bajo el limite en " $tiempo 
+fi
