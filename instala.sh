91a78b20fbfcc3a498de5251f9440a30a86cfa57
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-24 17:06:02 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -86,7 +86,7 @@ update_pak () {
 echo -ne " \033[1;31m[ ! ]  TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!  [ ! ]"
 echo -e ' '
 #fun_bar1 'apt-add-repository universe -y'
-fun_ba1
+fun_bar1
 #echo -e ' '
 #fun_bar1 'sudo add-apt-repository ppa:neurobin/ppa'
 echo -ne " \033[1;31m[ ! ] apt-get update"
