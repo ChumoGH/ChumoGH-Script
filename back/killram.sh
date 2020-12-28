2e485eaa16625278f2d2cc7081c104e8e08a7099
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-28 16:53:21 -0500

    Update killram.sh

--- back/killram.sh
+++ back/killram.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #Autor: Henry Chumo 
 #Alias : ChumoGH
+
 id1=$(free -h | grep -i mem | awk {'print $4'})
 idM=$(echo $id1 |sed 's/[A-Z]//g')
 idm=$(echo $idM |sed 's/[a-z]//g')
@@ -8,7 +9,18 @@ n=90
 tiempo=$(printf '%(%D-%H:%M:%S)T') 
 if [ "$n" -gt "$idm" ]; then 
 echo $idm "Mbs  -  Limpiando y Reiniciado el " $tiempo >> /root/lm.log
-source <(curl -sL https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/gnula.sh)
+echo 3 > /proc/sys/vm/drop_caches 1> /dev/null 2> /dev/null
+sysctl -w vm.drop_caches=3 1> /dev/null 2> /dev/null
+swapoff -a && swapon -a 1> /dev/null 2> /dev/null
+sudo sync
+sudo sysctl -w vm.drop_caches=3
+sysctl -w vm.drop_caches=3 > /dev/null 2>&1
+swapoff -a && swapon -a 1> /dev/null 2> /dev/null
+rm -rf /tmp/*
+sudo apt autoremove -y
+sudo apt purge & sudo apt clean 
+sudo rm /var/lib/apt/lists/lock && sudo rm /var/cache/apt/archives/lock && sudo rm /var/lib/dpkg/lock
+
 else 
 echo $n "Mbs - Esta bajo el limite " $id1 " el " $tiempo >> /root/lm.log
 fi
