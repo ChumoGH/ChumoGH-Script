1b12291323daad8742deeebec6ec9b9a0eda9148
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-29 01:34:49 -0500

    Update ssrrmu.sh

--- back/ssrrmu.sh
+++ back/ssrrmu.sh
@@ -1,6 +1,9 @@
-curl https://raw.githubusercontent.com/casitadelterror/dankeladm/master/new/ssrrmu.sh > /tmp/ssrrmu.sh
-sed -i "s;DANKELTHAHER;ChumoGH;g" /tmp/ssrrmu.sh
-sed -i "s;dankel;chumogh;g" /tmp/ssrrmu.sh
-chmod +x /tmp/ssrrmu.sh
-bash /tmp/ssrrmu.sh
+
+
+source <(curl -sL https://raw.githubusercontent.com/casitadelterror/dankeladm/master/new/ssrrmu.sh)
+#curl https://raw.githubusercontent.com/casitadelterror/dankeladm/master/new/ssrrmu.sh > /tmp/ssrrmu.sh
+#sed -i "s;DANKELTHAHER;ChumoGH;g" /tmp/ssrrmu.sh
+#sed -i "s;dankel;chumogh;g" /tmp/ssrrmu.sh
+#chmod +x /tmp/ssrrmu.sh
+#bash /tmp/ssrrmu.sh
 #sed '/gnula.sh/ d' /tmp/ssrrmu.sh > /bin/ejecutar/crontab
