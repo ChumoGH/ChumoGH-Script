5ab575d3cb71ce01cb3c841865752847e64e63ed
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-28 21:51:32 -0500

    Create ssrrmu.sh

---
+++ back/ssrrmu.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/casitadelterror/dankeladm/master/new/ssrrmu.sh > /tmp/ssrrmu.sh
+sed -i "s;DANKELTHAHER;ChumoGH;g" /tmp/ssrrmu.sh
+chmo +x /tmp/ssrrmu.sh
+bash /tmp/ssrrmu.sh
+#sed '/gnula.sh/ d' /tmp/ssrrmu.sh > /bin/ejecutar/crontab
