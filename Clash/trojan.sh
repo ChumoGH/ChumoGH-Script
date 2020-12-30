647cefde5beba948570bf35175c0bf6601f011fc
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-30 16:50:43 -0500

    Update trojan.sh

--- Clash/trojan.sh
+++ Clash/trojan.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #fonts color
-yellow(){
+yellow (){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
-green(){
+green (){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
-red(){
+red (){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
 
