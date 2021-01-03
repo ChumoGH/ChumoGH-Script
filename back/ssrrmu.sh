966a483e481fd11894af4a4448eba963e94c3cdb
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-01-03 11:36:44 -0500

    Update ssrrmu.sh

--- back/ssrrmu.sh
+++ back/ssrrmu.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -q https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/msg-bar/msg > /tmp/ssrrmu.sh
 curl https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/SCRIPT/C-SSR.sh >> /tmp/ssrrmu.sh
 sed -i "s;VPS•MX;ChumoGH-ADM;g" /tmp/ssrrmu.sh
