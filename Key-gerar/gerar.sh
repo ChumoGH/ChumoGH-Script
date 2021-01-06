e8ecd8a5bf3ddeb0f9636003950b01fd8e9c1c5c
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-01-05 22:58:27 -0500

    Update gerar.sh

--- Key-gerar/gerar.sh
+++ Key-gerar/gerar.sh
@@ -47,7 +47,7 @@ echo -e "\033[0;36m   Keygen:\033[0;35m 8888"
 fi`;
 [[ $v1 = $v2 ]] && vesaoSCT="\033[1;32m${txt[315]} \033[0;33m ($v2)" || vesaoSCT="\033[1;31m${txt[316]}\033[0;33m($v2) ► \033[1;32m($v1)\033[1;31m"
 echo -e "   ${cor[2]} ${vesaoSCT} ${cor[0]}"
-echo -e "\033[0;31mSystem:\033[0;32m$(cat /etc/issue.net) \033[0;31mIP:\033[0;32m $(wget -qO- ifconfig.me)"
+echo -e "   \033[0;31mSystem:\033[0;32m$(cat /etc/issue.net) \033[0;31mIP:\033[0;32m $(wget -qO- ifconfig.me)"
 msg -bar
 echo -ne "$(msg -verd "        Keys Usadas") $(msg -azu " : ") " && msg -bra "\033[1;41m $(cat $IVAR) $porta"
 }
