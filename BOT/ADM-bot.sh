c3953942083359e2fc812f178277337f97c99126
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-01-14 12:48:04 -0500

    Create ADM-bot.sh

---
+++ BOT/ADM-bot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo ""
+curl  https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/msg-bar/msg > /tmp/bot.sh
+curl  https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Parches-TEM/ADMbot.sh >> /tmp/bot.sh
+sed -i "s;VPS•MX;ChumoGH-ADM;g" /tmp/bot.sh
+sed -i "s;@Kalix1;ChumoGH;g" /tmp/bot.sh
+sed -i "s;VPS-MX;chumogh;g" /tmp/bot.sh
+chmod +x /tmp/ssrrmu.sh && bash /tmp/bot.sh
