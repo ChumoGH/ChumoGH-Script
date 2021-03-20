16aa436d5b556177ffad693fbd72799ad1b27c8b
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-03-19 20:25:13 -0500

    Delete BOT directory

--- BOT/ADM-bot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ""
-curl  https://raw.githubusercontent.com/ChumoGH/ChumoGH-Script/master/msg-bar/msg > /tmp/bot.sh
-curl  https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Parches-TEM/ADMbot.sh >> /tmp/bot.sh
-sed -i "s;VPS•MX;ChumoGH-ADM;g" /tmp/bot.sh
-sed -i "s;@Kalix1;ChumoGH;g" /tmp/bot.sh
-sed -i "s;VPS-MX;chumogh;g" /tmp/bot.sh
-chmod +x /tmp/bot.sh && bash /tmp/bot.sh
