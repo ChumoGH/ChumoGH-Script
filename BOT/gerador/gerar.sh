50cbe5012cf8d0fc0b471e390575893849e19665
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2021-03-20 02:19:59 -0500

    Update gerar.sh

--- BOT/gerador/gerar.sh
+++ BOT/gerador/gerar.sh
@@ -225,16 +225,16 @@ number=$(expr length $1)
 for((i=1; i<$number+1; i++)); do
 txt[$i]=$(echo "$1" | cut -b $i)
 case ${txt[$i]} in
-".")txt[$i]="*";;
-"*")txt[$i]=".";;
+".")txt[$i]="+";;
+"+")txt[$i]=".";;
 "1")txt[$i]="@";;
 "@")txt[$i]="1";;
 "2")txt[$i]="?";;
 "?")txt[$i]="2";;
-"4")txt[$i]="%";;
-"%")txt[$i]="4";;
-"-")txt[$i]="K";;
-"K")txt[$i]="-";;
+"3")txt[$i]="%";;
+"%")txt[$i]="3";;
+"/")txt[$i]="K";;
+"K")txt[$i]="/";;
 esac
 txtofus+="${txt[$i]}"
 done
