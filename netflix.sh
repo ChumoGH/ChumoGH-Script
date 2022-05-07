59c97431331e1281f31dfe0e9e9d384e5ed49c31
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2022-05-07 13:56:40 -0500

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -35,10 +35,11 @@ tar xf badvpn-1.999.128.tar.bz2
 cd /etc/adm-lite/badvpn-1.999.128
 cmake /etc/adm-lite/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 make install
-badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-connections-for-client 1000 --client-socket-sndbuf 0 --udp-mtu 9000 > /dev/nul &
-echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-connections-for-client 1000 --client-socket-sndbuf 0 --udp-mtu 9000 > /dev/nul &' > /etc/rc.local
+badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-connections-for-client 1000 --max-connections-for-client 50 > /dev/nul &
+echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-connections-for-client 1000 --max-connections-for-client 50 > /dev/nul &' > /etc/rc.local
 #echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &' >> /etc/rc.local
 cd /etc/adm-lite/
+[[ -d badvpn-1.999.128/ ]] && rm -rf badvpn-1.999.128
 rm -f badvpn-1.999.128.tar.*
 rm -f install > /dev/nul
 echo "Finalizado"
